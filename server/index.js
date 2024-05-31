d833dfe611ba16a54c0412799f356ac05635b218
aminmustafayev <aminmustafayev778@gmail.com>
2024-05-31 13:41:06 +0400

    back root completed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 5050;
+var cors = require("cors");
+app.use(cors());
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const mongoose = require("mongoose");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const MySchema = new mongoose.Schema({
+    title: String,
+    img: String,
+    price:Number,
+    desc: String,
+});
+const MyModel = mongoose.model("MyModel", MySchema);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", async (req, res) => {
+    const found = await MyModel.find();
+    try {
+        res.send(found);
+    } catch {
+        res.send("cant get");
+    }
+});
+app.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const found = await MyModel.findById(id);
+    try {
+        res.send(found);
+    } catch {
+        res.send("cant get");
+    }
+});
 
+app.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const found = await MyModel.findByIdAndDelete(id);
+    try {
+        res.send(found);
+    } catch {
+        res.send("cant del");
+    }
+});
+
+app.post("/", async (req, res) => {
+    const payl = new MyModel(req.body);
+    await payl.save();
+    try {
+        res.send(payl);
+    } catch {
+        res.send("cant get");
+    }
+});
+
+const CONNECTION = "mongodb+srv://Rena:Rena123@timezone.3rdbh5l.mongodb.net/";
+mongoose.connect(CONNECTION).then(() => {
+    console.log("connected");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
